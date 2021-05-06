13a438b3369ce23280f263d1d013d8e4b51df895
Laplace-Demon19 <83784287+Laplace-Demon19@users.noreply.github.com>
2021-05-07 01:07:24 +0600

    Create Vzlom.py

---
+++ Vzlom.py
@@ -0,0 +1,317 @@
+#made by @VadimSap & KeyZenD
+import asyncio
+import re
+import time
+from time import sleep
+from userbot import CMD_HELP, ZALG_LIST
+from userbot.events import register
+
+@register(outgoing=True, pattern='^.vzlom(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	await typew.edit("`идёт Взлом вашей жопы...`")
+	sleep(1)
+	await typew.edit("`Взлом жопы: `0%")
+	number = 1
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██▉")
+	number = number+ 1 
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   █████████████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ██████████████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████████▊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ███████████████▉")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████████")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████████▎")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████████▍")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████████▌")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Взлом жопы: `") +str(number) + "%   ████████████████▌")
+	sleep(1)
+	await typew.edit("`Взлом жопы....`")
+	sleep(1)
+	await typew.edit("`Все́ пиздец ваша жопа взломана теперь в неё может войти кто Угодно!`")
